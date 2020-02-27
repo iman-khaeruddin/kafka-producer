b0a2428bd9739051c27885e3fbda312c78bdab17
iman_khaeruddin <iman.khaeruddin@infinetworks.com>
2020-02-27 11:16:04 +0700

    initial commit

---
+++ src/main/java/com/example/kafka/config/KakfaConfiguration.java
@@ -0,0 +1,59 @@
+package com.example.kafka.config;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import com.example.kafka.model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KakfaConfiguration {
+
+    /**
+    * This configuration for produce an object or json
+    * */
+    @Bean
+    public ProducerFactory<String, User> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    /**
+     * This configuration for produce a string or plain text
+     * */
+    @Bean
+    public ProducerFactory<String, String> producerFactory2() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+
+    @Bean
+    public KafkaTemplate<String, User> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+    
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate2() {
+        return new KafkaTemplate<>(producerFactory2());
+    }
+
+
+}
---
+++ src/main/java/com/example/kafka/controller/SampleController.java
@@ -0,0 +1,49 @@
+package com.example.kafka.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.kafka.model.User;
+
+@RestController
+@RequestMapping("kafka/")
+public class SampleController {
+
+	@Autowired
+	private KafkaTemplate<String, User> kafkaTemplate;
+	
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate2;
+	
+	private String TOPIC = "test1";
+
+	/**
+	 * @param name
+	 *
+	 * Publish user object.
+	 * */
+	@GetMapping("publish/json/{name}")
+	public String Post1(@PathVariable("name") String name) {
+		
+		kafkaTemplate.send(TOPIC , new User(name, "TECH"));
+		
+		return "publish successfully";
+	}
+
+	/**
+	 * @param message
+	 *
+	 * Publish string.
+	 * */
+	@GetMapping("publish/{message}")
+	public String Post2(@PathVariable("message") String message) {
+		
+		kafkaTemplate2.send(TOPIC , message);
+		
+		return "publish successfully";
+	}
+}
